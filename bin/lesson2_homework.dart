555503740b0b4fe331f0b4b697b36c7b90dbc45f
NickDarby77 <132682600+NickDarby77@users.noreply.github.com>
2023-05-19 04:33:45 +0600

    first commit

---
+++ bin/lesson2_homework.dart
@@ -0,0 +1,17 @@
+void main(){
+  // task #1
+  double b = 4.2;
+  int a = 3;
+  print(a + b);
+
+  // task #2
+  String text = 'Name';
+  text = 'Flutter';
+  print('I want to learn $text');
+
+  // task #3
+  double myNumber = 2.225;
+  String myText;
+  myText = myNumber.toString();
+  print(myText);
+}
